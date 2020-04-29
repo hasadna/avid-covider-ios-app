589ae9c12c6d7643e5cc309bcc882588a40ec945
Adar Hefer <adar.hefer@gmail.com>
2020-04-29 15:32:33 +0300

    Initial Commit

---
+++ DailyCorona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyCorona
+//
+//  Created by Adar Hefer on 29/04/2020.
+//  Copyright © 2020 Adar Hefer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
